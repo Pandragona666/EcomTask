fe8bcc0681faadb8f12f4c393797e63c7347d2d9
Pandragona <asiuspospolitus@gmail.com>
2019-09-20 15:09:49 +0200

    application icon changed to unicorn

--- EcomLocatorV2.Android/Resources/Resource.designer.cs
+++ EcomLocatorV2.Android/Resources/Resource.designer.cs
@@ -5584,26 +5584,26 @@ namespace EcomLocatorV2.Droid
 			// aapt resource value: 0x7f020054
 			public const int avd_hide_password = 2130837588;
 			
-			// aapt resource value: 0x7f02013a
-			public const int avd_hide_password_1 = 2130837818;
-			
 			// aapt resource value: 0x7f02013b
-			public const int avd_hide_password_2 = 2130837819;
+			public const int avd_hide_password_1 = 2130837819;
 			
 			// aapt resource value: 0x7f02013c
-			public const int avd_hide_password_3 = 2130837820;
+			public const int avd_hide_password_2 = 2130837820;
+			
+			// aapt resource value: 0x7f02013d
+			public const int avd_hide_password_3 = 2130837821;
 			
 			// aapt resource value: 0x7f020055
 			public const int avd_show_password = 2130837589;
 			
-			// aapt resource value: 0x7f02013d
-			public const int avd_show_password_1 = 2130837821;
-			
 			// aapt resource value: 0x7f02013e
-			public const int avd_show_password_2 = 2130837822;
+			public const int avd_show_password_1 = 2130837822;
 			
 			// aapt resource value: 0x7f02013f
-			public const int avd_show_password_3 = 2130837823;
+			public const int avd_show_password_2 = 2130837823;
+			
+			// aapt resource value: 0x7f020140
+			public const int avd_show_password_3 = 2130837824;
 			
 			// aapt resource value: 0x7f020056
 			public const int background = 2130837590;
@@ -6268,11 +6268,11 @@ namespace EcomLocatorV2.Droid
 			// aapt resource value: 0x7f020132
 			public const int notification_icon_background = 2130837810;
 			
-			// aapt resource value: 0x7f020138
-			public const int notification_template_icon_bg = 2130837816;
-			
 			// aapt resource value: 0x7f020139
-			public const int notification_template_icon_low_bg = 2130837817;
+			public const int notification_template_icon_bg = 2130837817;
+			
+			// aapt resource value: 0x7f02013a
+			public const int notification_template_icon_low_bg = 2130837818;
 			
 			// aapt resource value: 0x7f020133
 			public const int notification_tile_bg = 2130837811;
@@ -6288,6 +6288,9 @@ namespace EcomLocatorV2.Droid
 			
 			// aapt resource value: 0x7f020137
 			public const int tooltip_frame_light = 2130837815;
+			
+			// aapt resource value: 0x7f020138
+			public const int unicornIcon = 2130837816;
 			
 			static Drawable()
 			{
